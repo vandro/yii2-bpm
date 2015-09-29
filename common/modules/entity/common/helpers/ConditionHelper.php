45675d9f8f3063b2f9ee728ee5bc0d10ccb5a837
Avazbek Niyazov <avazbe@gmail.com>
2015-09-29 11:29:53 +0500

    add condition processing

--- common/modules/entity/common/helpers/ConditionHelper.php
+++ common/modules/entity/common/helpers/ConditionHelper.php
@@ -24,12 +24,31 @@ class ConditionHelper
         switch($condition->operator){
             case 'equal':
                 return self::equal($condition);
-            case 'notequal':
+            case 'not_equal':
                 return self::notEqual($condition);
+            case 'identical':
+                return self::identical($condition);
+            case 'not_identical':
+                return self::notIdentical($condition);
+            case 'greater_than':
+                return self::greaterThan($condition);
+            case 'less_than':
+                return self::lessThan($condition);
+            case 'greater_than_or_equal_to':
+                return self::greaterThanOrEqualTo($condition);
+            case 'less_than_or_equal_to':
+                return self::lessThanOrEqualTo($condition);
+            case 'contains':
+                return self::contains($condition);
+            case 'not_contains':
+                return self::notContains($condition);
+            case 'begin_with':
+                return self::beginWith($condition);
+            case 'end_with':
+                return self::endWith($condition);
             default:
                 return false;
         }
-        return self::$condition;
     }
 
     protected static function setEntityFieldValue($condition, $taskId)
@@ -50,11 +69,67 @@ class ConditionHelper
 
     protected static function equal($condition)
     {
-        return self::getEntityFieldValue($condition) == $condition->operand2;
+        return (self::$fieldValue == $condition->operand_2);
     }
 
     protected static function notEqual($condition)
     {
-        return self::getEntityFieldValue($condition) != $condition->operand2;
+        return (self::$fieldValue != $condition->operand_2);
+    }
+
+    public static function identical($condition)
+    {
+        return (self::$fieldValue === $condition->operand_2);
+    }
+
+    public static function notIdentical($condition)
+    {
+        return (self::$fieldValue !== $condition->operand_2);
+    }
+
+    public static function greaterThan($condition)
+    {
+        return (self::$fieldValue > $condition->operand_2);
+    }
+
+    public static function lessThan($condition)
+    {
+        return (self::$fieldValue < $condition->operand_2);
+    }
+
+    public static function greaterThanOrEqualTo($condition)
+    {
+        return (self::$fieldValue >= $condition->operand_2);
+    }
+
+    public static function lessThanOrEqualTo($condition)
+    {
+        return (self::$fieldValue <= $condition->operand_2);
+    }
+
+    public static function contains($condition)
+    {
+        return (strpos(self::$fieldValue,$condition->operand_2) == false?false:true);
+    }
+
+    public static function notContains($condition)
+    {
+        return (strpos(self::$fieldValue,$condition->operand_2) != false?false:true);
+    }
+
+    public static function beginWith($condition)
+    {
+        $length = strlen($condition->operand_2);
+        return (substr(self::$fieldValue, 0, $length) === $condition->operand_2);
+    }
+
+    public static function endWith($condition)
+    {
+        $length = strlen($condition->operand_2);
+        if ($length == 0) {
+            return true;
+        }
+
+        return (substr(self::$fieldValue, -$length) === $condition->operand_2);
     }
 }
