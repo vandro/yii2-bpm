b0af1a944e5e431f134ddb9e5e8e22ab0f9f25db
Avazbek Niyazov <avazbe@gmail.com>
2015-10-04 14:13:44 +0500

    add the upload module

--- common/modules/entity/frontend/views/actions-cart/entity/create.php
+++ common/modules/entity/frontend/views/actions-cart/entity/create.php
@@ -25,6 +25,9 @@ use yii\helpers\Html;
         <?= $this->render('_form', [
             'formModel' => $formModel,
             'entity' => $entity,
+            'task_id' => $task_id,
+            'node_id' => $node_id,
+            'action_id' => $action_id,
         ]) ?>
     </div>
 </div>
