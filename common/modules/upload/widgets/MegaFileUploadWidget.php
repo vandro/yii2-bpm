68554103b2874247d8550d28cba7adc5be33e76a
Avazbek Niyazov <avazbe@gmail.com>
2015-10-04 14:13:15 +0500

    add the upload module

--- common/modules/upload/widgets/MegaFileUploadWidget.php
+++ common/modules/upload/widgets/MegaFileUploadWidget.php
@@ -16,18 +16,33 @@ class MegaFileUploadWidget extends \yii\bootstrap\Widget
     public $hoverClass = 'hover';
     public $focusClass = 'focus';
     public $responseType = 'json';
+    public $taskId = null;
+    public $nodeId = null;
+    public $actionId = null;
 
     public function run(){
 
+        $params = [];
+        if(!empty($this->taskId)){
+            $params['task_id'] = $this->taskId;
+        }
+
+        if(!empty($this->nodeId)){
+            $params['node_id'] = $this->nodeId;
+        }
+
+        if(!empty($this->actionId)){
+            $params['action_id'] = $this->actionId;
+        }
+
         return $this->render('megaFileUploadWidgetView',[
-            'url' => Yii::$app->getUrlManager()->createAbsoluteUrl('upload/do/file-upload'),
+            'url' => Yii::$app->getUrlManager()->createAbsoluteUrl(array_merge(['upload/do/file-upload'], $params)),
             'progressUrl' => Yii::$app->getUrlManager()->createAbsoluteUrl('upload/do/upload-progress'),
             'name' => $this->name,
             'multipart' => $this->multipart,
             'hoverClass' => $this->hoverClass,
             'focusClass' => $this->focusClass,
             'responseType' => $this->responseType,
-
         ]);
     }
 
