33c51a2912539c31d2f1599384619c6d24f68fbe
Avazbek Niyazov <avazbe@gmail.com>
2015-10-06 16:28:33 +0500

    add the epigu  module

--- backend/config/main.php
+++ backend/config/main.php
@@ -15,6 +15,9 @@ return [
         'entity' => [
             'class' => 'common\modules\entity\backend\Module',
         ],
+        'epigu' => [
+            'class' => 'common\modules\epigu\Module',
+        ],
     ],
     'components' => [
         'view' => [
