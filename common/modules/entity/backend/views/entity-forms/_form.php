0635720d0f200cfa83a56c45937487295a5ad142
Avazbek Niyazov <avazbe@gmail.com>
2015-10-08 13:26:51 +0500

    fix the html field widget

--- common/modules/entity/backend/views/entity-forms/_form.php
+++ common/modules/entity/backend/views/entity-forms/_form.php
@@ -19,45 +19,50 @@ use yii\web\JsExpression;
 
     <?= $form->field($model, 'code')->textInput() ?>
 
-<!--    --><?//= $form->field($model, 'html')->textarea(['rows' => 10]) ?>
-
-    <?= $form->field($model, 'html')->widget(
-        CodemirrorWidget::className(),
-        [
-            'assets'=>[
-                CodemirrorAsset::MODE_JAVASCRIPT,
-                CodemirrorAsset::MODE_HTTP,
-                CodemirrorAsset::MODE_CSS,
-                CodemirrorAsset::MODE_PHP,
-                CodemirrorAsset::MODE_XML,
-                CodemirrorAsset::MODE_HTMLMIXED,
-                CodemirrorAsset::MODE_CLIKE,
-                CodemirrorAsset::ADDON_COMMENT,
-                CodemirrorAsset::ADDON_DISPLAY_FULLSCREEN,
-                CodemirrorAsset::THEME_ECLIPSE,
-            ],
-            'settings'=>[
-                'lineNumbers' => true,
-                'matchBrackets' => true,
-                'mode' => "text/html", //"application/x-httpd-php-open",
-                'indentUnit' => 4,
-                'indentWithTabs' => true,
-                'extraKeys' => [
-                    "F11" => new JsExpression("function(cm){cm.setOption('fullScreen', !cm.getOption('fullScreen'));}"),
-                    "Esc" => new JsExpression("function(cm){if(cm.getOption('fullScreen')) cm.setOption('fullScreen', false);}"),
-                ],
-            ],
-        ]
-    ); ?>
-
-    <?='Available fields codes:'?>
-    <?php foreach($model->fields as $field){
-        echo ' {%'.$field->code.'%} ';
-    }?>
-
     <?= $form->field($model, 'mode')->dropDownList(['create' => 'Create new entity item', 'update' => 'Update existed entity item'], ['prompt' => 'Choose form work mode']); ?>
 
-    <?= $form->field($model, 'entity_id')->hiddenInput()->label(''); ?>
+    <?php if(!empty($model->fields) || !empty($model->fields)){?>
+
+        <?= $form->field($model, 'html')->widget(
+            CodemirrorWidget::className(),
+            [
+                'assets'=>[
+                    CodemirrorAsset::MODE_JAVASCRIPT,
+                    CodemirrorAsset::MODE_HTTP,
+                    CodemirrorAsset::MODE_CSS,
+                    CodemirrorAsset::MODE_PHP,
+                    CodemirrorAsset::MODE_XML,
+                    CodemirrorAsset::MODE_HTMLMIXED,
+                    CodemirrorAsset::MODE_CLIKE,
+                    CodemirrorAsset::ADDON_COMMENT,
+                    CodemirrorAsset::ADDON_DISPLAY_FULLSCREEN,
+                    CodemirrorAsset::THEME_ECLIPSE,
+                ],
+                'settings'=>[
+                    'lineNumbers' => true,
+                    'matchBrackets' => true,
+                    'mode' => "text/html", //"application/x-httpd-php-open",
+                    'indentUnit' => 4,
+                    'indentWithTabs' => true,
+                    'extraKeys' => [
+                        "F11" => new JsExpression("function(cm){cm.setOption('fullScreen', !cm.getOption('fullScreen'));}"),
+                        "Esc" => new JsExpression("function(cm){if(cm.getOption('fullScreen')) cm.setOption('fullScreen', false);}"),
+                    ],
+                ],
+            ]
+        ); ?>
+
+        Available fields codes:
+        <?php foreach($model->fields as $field){
+            echo ' {%'.$field->code.'%} ';
+        }?>
+        <br>
+        Available child form codes:
+        <?php foreach($model->childForms as $childForm){
+            echo ' {%'.$childForm->code.'%} ';
+        }?>
+
+    <?php } ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
