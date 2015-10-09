a93bf6686c427c63c91f460aee785f09afaf14e0
Avazbek Niyazov <avazbe@gmail.com>
2015-10-09 20:26:08 +0500

    add the  child forms

---
+++ common/modules/entity/common/components/ChildEntity.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a_niyazov
+ * Date: 09.10.2015
+ * Time: 20:21
+ */
+namespace common\modules\entity\common\components;
+
+class ChildEntity extends \common\modules\entity\common\components\Entity {}
