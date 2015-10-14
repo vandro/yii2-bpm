8b83eac109ca10f5b46e9e5ae1abe4ec2d601d82
Avazbek Niyazov <avazbe@gmail.com>
2015-10-14 17:59:08 +0500

    add parent_view_id

---
+++ common/modules/entity/backend/views/entity-views/_childViewsIndex.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a_niyazov
+ * Date: 01.09.2015
+ * Time: 14:32
+ */
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use common\helpers\ActionColumnHelper;
+use common\modules\entity\common\models\EntityFields;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\modules\entity\common\models\EntityTypesLangSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+
+?>
+<div class="entity-types-lang-index">
+<br>
+
+    <?= GridView::widget([
+        'dataProvider' => $model->getChildViewsAdp(),
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'title',
+            'code',
+        ],
+    ]); ?>
+</div>
