309dc065da4f3be89cd9b829ebd72292c5176ce3
Avazbek Niyazov <avazbe@gmail.com>
2015-10-15 18:51:00 +0500

    add the ActiveRecordClassFactory for entities

--- common/modules/entity/frontend/controllers/TestTasksController.php
+++ common/modules/entity/frontend/controllers/TestTasksController.php
@@ -3,6 +3,13 @@
 namespace common\modules\entity\frontend\controllers;
 
 //use common\modules\entity\common\factories\BehaviorClassFactory;
+use common\modules\entity\common\factories\ActiveRecordClassFactory;
+use common\modules\entity\common\factories\ActiveRecordStringRuleFactory;
+use common\modules\entity\common\factories\EntityClassFactory;
+use common\modules\entity\common\factories\EntityFormClassFactory;
+use common\modules\entity\common\models\EntityForms;
+use common\modules\entity\common\models\EntityTypes;
+use common\modules\entity\common\models\NodesActions;
 use common\modules\entity\common\models\smi\SmiFounders;
 use common\modules\entity\common\models\smi\SmiReestr;
 use common\modules\entity\common\models\smi\SmiSpecialization;
@@ -152,15 +159,158 @@ class TestTasksController extends Controller
         file_put_contents(Yii::$app->basePath.'/../common/modules/entity/common/behaviors/RequestBehaviors.php',BehaviorClassFactory::getClassString($params));
     }
 
-    public function actionTest()
+    public function actionCreateEntity($id)
     {
-        $specializations = SmiSpecialization::find()->all();
-        $founders = SmiFounders::find()->all();
 
-        foreach($founders as $founder) {
-            DebugHelper::printSingleObject(count(SmiReestr::find()->founder($founder)->all()));
+        if (EntityClassFactory::generateFile($id)) {
+            echo 'Generated';
+        } else {
+            echo 'Not';
         }
-//        return $this->render('index2');
+    }
+
+    public function actionTestCreateEntity()
+    {
+        $params = [
+            ActiveRecordClassFactory::NAME_SPACE => 'common\modules\entity\common\entities',
+            ActiveRecordClassFactory::CLASS_NAME => 'RequestRelation',
+            ActiveRecordClassFactory::ACTIVE_QUERY_CLASS_NAME => 'RequestQuery',
+            ActiveRecordClassFactory::TABLE_NAME => 'request',
+            ActiveRecordClassFactory::AUTHOR_NAME => 'Avazbek Niyazov',
+            ActiveRecordClassFactory::I18N_MESSAGE_FILE_ALIAS => 'app',
+            ActiveRecordClassFactory::DATABASE_NAME => 'pdb',
+            ActiveRecordClassFactory::CLASS_FILE_LOCATION_PATH => Yii::$app->basePath.'/../common/modules/entity/common/entities/',
+            ActiveRecordClassFactory::PROPERTIES_VALIDATION_RULES => [
+                'string' => [
+                    ActiveRecordClassFactory::CLASS_NAME => '\common\modules\entity\common\factories\ActiveRecordStringRuleFactory',
+                ],
+                'integer' => [
+                    ActiveRecordClassFactory::CLASS_NAME => '\common\modules\entity\common\factories\ActiveRecordIntegerRuleFactory',
+                ],
+                'email' => [
+                    ActiveRecordClassFactory::CLASS_NAME => '\common\modules\entity\common\factories\ActiveRecordEmailRuleFactory',
+                ],
+            ],
+            ActiveRecordClassFactory::PROPERTIES => [
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'title_string',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'string',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Наименование',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'string',
+                            ActiveRecordStringRuleFactory::MAX => 255,
+                        ],
+                    ],
+                ],
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'entity_type_id',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'integer',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Тип сущности',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'integer',
+                        ],
+                    ],
+                    ActiveRecordClassFactory::PROPERTY_RELATION => [
+                        ActiveRecordClassFactory::PROPERTY_RELATION_METHOD_NAME => 'EntityType',
+                        ActiveRecordClassFactory::PROPERTY_RELATION_TYPE => ActiveRecordClassFactory::RELATION_TYPE_HAS_MANY,
+                        ActiveRecordClassFactory::PROPERTY_RELATION_FOREIGN_KEY => 'entity_type_id',
+                        ActiveRecordClassFactory::PROPERTY_RELATION_TARGET_KEY => 'id',
+                        ActiveRecordClassFactory::PROPERTY_RELATION_TARGET_CLASS => 'EntityType',
+                    ],
+                ],
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'name',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'string',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Наименование',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'string',
+                        ],
+                    ]
+                ],
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'code_string',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'string',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Код',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'string',
+                            ActiveRecordStringRuleFactory::MAX => 255,
+                            ActiveRecordStringRuleFactory::MIN => 50,
+                        ],
+                    ]
+                ],
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'balans',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'integer',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Баланс',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'integer',
+                            ActiveRecordStringRuleFactory::MAX => 150,
+                        ],
+                    ]
+                ],
+                [
+                    ActiveRecordClassFactory::PROPERTY_NAME => 'email',
+                    ActiveRecordClassFactory::PROPERTY_TYPE => 'string',
+                    ActiveRecordClassFactory::PROPERTY_LABEL => 'Адрес электронной почты',
+                    ActiveRecordClassFactory::PROPERTY_VALIDATION_RULES => [
+                        [
+                            ActiveRecordClassFactory::PROPERTY_VALIDATION_RULE_TYPE => 'email',
+                        ],
+                    ]
+                ],
+            ],
+        ];
+
+        if(ActiveRecordClassFactory::generateClassFile($params)){
+            echo 'Generated';
+        }else{
+            echo 'Not';
+        }
+
+
+//        DebugHelper::printSingleObject(BehaviorClassFactory::getClassString($params));
+
+//        file_put_contents(Yii::$app->basePath.'/../common/modules/entity/common/entities/RequestEntity.php',ActiveRecordClassFactory::getClassString($params));
+    }
+
+    public function getName($nameString)
+    {
+        $name = '';
+        $arName = explode("_",$nameString);
+        foreach($arName as $nameItem){
+            $name .= ucfirst($nameItem);
+        }
+
+        return $name;
+    }
+
+    public function actionTestEntityClass($id)
+    {
+        $entityType = EntityTypes::findOne($id);
+        $className = '\\common\\modules\\entity\\common\\entities\\'.$this->getName($entityType->code);
+        DebugHelper::printActiveRecordsArray($className::find()->all());
+    }
+
+    public function actionTestEntityFormClass($id)
+    {
+        $entityForm = EntityForms::findOne($id);
+        $className = '\\common\\modules\\entity\\common\\entities\\forms\\'.$this->getName($entityForm->code).'Form';
+        DebugHelper::printActiveRecordsArray($className::find()->all());
+    }
+
+    public function actionCreateEntityForm($id)
+    {
+
+        if (EntityFormClassFactory::generateFile($id)) {
+            echo 'Generated';
+        } else {
+            echo 'Not';
+        }
     }
 
     public function actionDraw()
@@ -171,5 +321,11 @@ class TestTasksController extends Controller
     public function actionConfig($id)
     {
         DebugHelper::printSingleObject(json_decode(file_get_contents('http://official2.gov.uz/getConfig/'.$id)));
+    }
+
+    public function actionGetHandlers($id)
+    {
+        $action = NodesActions::findOne($id);
+        DebugHelper::printSingleObject($action->handlers);
     }
 }
