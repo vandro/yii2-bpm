98412db20e878c5e8dfff67ca5a80e6f462006ea
Avazbek Niyazov <avazbe@gmail.com>
2015-10-15 18:50:05 +0500

    add the ActiveRecordClassFactory for entities

--- common/modules/entity/common/factories/ActiveRecordClassFactory.php
+++ common/modules/entity/common/factories/ActiveRecordClassFactory.php
@@ -20,6 +20,14 @@ class ActiveRecordClassFactory
     const PROPERTY_TYPE = 'PROPERTY_TYPE';
     const PROPERTY_VALIDATION_RULES = 'PROPERTY_VALIDATION_RULES';
     const PROPERTY_VALIDATION_RULE_TYPE = 'PROPERTY_VALIDATION_RULE_TYPE';
+    const PROPERTY_RELATION = 'PROPERTY_RELATION';
+    const PROPERTY_RELATION_METHOD_NAME = 'PROPERTY_RELATION_METHOD_NAME';
+    const PROPERTY_RELATION_TYPE = 'PROPERTY_RELATION_TYPE';
+    const RELATION_TYPE_HAS_MANY = 'hasMany';
+    const RELATION_TYPE_HAS_ONE = 'hasOne';
+    const PROPERTY_RELATION_FOREIGN_KEY = 'PROPERTY_RELATION_FOREIGN_KEY';
+    const PROPERTY_RELATION_TARGET_KEY = 'PROPERTY_RELATION_TARGET_KEY';
+    const PROPERTY_RELATION_TARGET_CLASS = 'PROPERTY_RELATION_TARGET_CLASS';
     const AUTHOR_NAME = 'AUTHOR_NAME';
     const PROPERTY_LABEL = 'PROPERTY_LABEL';
     const I18N_MESSAGE_FILE_ALIAS = 'I18N_MESSAGE_FILE_ALIAS';
@@ -59,6 +67,7 @@ class ActiveRecordClassFactory
         self::rGetDb();
         self::rRules();
         self::rAttributeLabels();
+        self::rRelationMethods();
         //self::rFind();
         self::rClassEnd();
     }
@@ -227,6 +236,22 @@ class ActiveRecordClassFactory
             self::$classString .= "    {\n";
             self::$classString .= "         return new ".self::$params[self::ACTIVE_QUERY_CLASS_NAME]."(get_called_class());\n";
             self::$classString .= "    }\n\n";
+        }
+    }
+
+    protected static function rRelationMethods()
+    {
+        foreach(self::$params[self::PROPERTIES] as $property) {
+            if(isset($property[self::PROPERTY_RELATION])) {
+                $relation = $property[self::PROPERTY_RELATION];
+                self::$classString .= "    /**\n";
+                self::$classString .= "     * @return \\yii\\db\\ActiveQuery.\n";
+                self::$classString .= "     */\n";
+                self::$classString .= "    public function get" . $relation[self::PROPERTY_RELATION_METHOD_NAME] . "()\n";
+                self::$classString .= "    {\n";
+                self::$classString .= "         return $" . "this->" . $relation[self::PROPERTY_RELATION_TYPE] . "(" . $relation[self::PROPERTY_RELATION_TARGET_CLASS] . "::className(),['" . $relation[self::PROPERTY_RELATION_TARGET_KEY] . "' => '" . $relation[self::PROPERTY_RELATION_FOREIGN_KEY] . "']);\n";
+                self::$classString .= "    }\n\n";
+            }
         }
     }
 
