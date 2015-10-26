4b0797eb8b7300b34262e094296f22383d1a70b1
Avazbek Niyazov <avazbe@gmail.com>
2015-10-26 16:39:21 +0500

    add first_name and last_name to user

--- common/modules/entity/common/models/User.php
+++ common/modules/entity/common/models/User.php
@@ -174,4 +174,13 @@ class User extends \yii\db\ActiveRecord
 
         return $dataProvider;
     }
+
+    public function getTitle()
+    {
+        $title = '';
+        if(!empty($this->first_name)) $title .= $this->first_name." ";
+        if(!empty($this->first_name)) $title .= $this->last_name." ";
+        $title .= "(".$this->username.")";
+        return $title;
+    }
 }
