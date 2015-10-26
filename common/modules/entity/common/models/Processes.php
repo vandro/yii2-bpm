a1b9247ffffdfd84f652f7de2f8c0db8778c5b8a
Avazbek Niyazov <avazbe@gmail.com>
2015-10-26 19:49:21 +0500

    add the graphic schema of the process

--- common/modules/entity/common/models/Processes.php
+++ common/modules/entity/common/models/Processes.php
@@ -66,6 +66,87 @@ class Processes extends \yii\db\ActiveRecord
         return $this->hasMany(ProcessNodes::className(), ['process_id' => 'id']);
     }
 
+    public function getJsonNodesArray()
+    {
+        $arNodes = [];
+
+        foreach($this->nodes as $node){
+            if($node->order_status == 'last'){
+                $borderWidth = 4;
+                $shape = 'circle';
+                $size = 60;
+                $background = '#dd5555';
+            }elseif($node->order_status == 'first'){
+                $borderWidth = 4;
+                $shape = 'circle';
+                $size = 60;
+                $background = '#ffffff';
+            }elseif($node->order_status == 'filling'){
+                $borderWidth = 4;
+                $shape = 'circle';
+                $size = 40;
+                $background = 'lightgreen';
+            }elseif($node->order_status == 'first_process'){
+                $borderWidth = 4;
+                $shape = 'box';
+                $size = 60;
+                $background = '#dd5555';
+            }elseif($node->order_status == 'inactive'){
+                $borderWidth = 4;
+                $shape = 'box';
+                $size = 60;
+                $background = '#ffb928';
+            }else{
+                $borderWidth = 3;
+                $shape = 'circle';
+                $size = 35;
+                $background = '#3399f3';
+            }
+
+            $arNodes[] = [
+                'id' => $node->id,
+                'label' => $node->id,
+                'title' => $node->title,
+                'shape' => $shape,
+                'font' => [
+                    'size' => $size,
+                    'color' => '#000000',
+                ],
+                'size' => $size,
+                'color' => [
+                    'background' => $background,
+                    'border' => '#002240'
+                ],
+                'borderWidth' => $borderWidth,
+            ];
+        }
+
+        return json_encode($arNodes);
+    }
+
+    public function getJsonEdgesArray()
+    {
+        $arEdges = [];
+        foreach($this->nodes as $node){
+            foreach($node->actionRoleLinks as $link) {
+                $arEdges[] = [
+                    'title' => $link->action->title."(".$link->role->title.")",
+                    'label' => $link->id,
+                    'from' => $node->id,
+                    'to' => $link->next_node_id,
+                    'arrows' => 'to',
+                    'length' => 200,
+                    'font' => [
+                        'size' => 30,
+                        'align' => 'bottom'
+                    ],
+                ];
+            }
+        }
+
+        return json_encode($arEdges);
+    }
+
     /**
      * Creates data provider instance with search query applied
      *
