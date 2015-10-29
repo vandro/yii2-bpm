cebec51a6eaeff8b125eedf18f37f245cad10877
Avazbek Niyazov <avazbe@gmail.com>
2015-10-29 10:21:20 +0500

    add date rule

---
+++ common/modules/generator/rules/ActiveRecordDateRuleGenerator.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Author: Avazbek Niyazov
+ * Email: avazbe@gmail.com
+ * Date: 23.09.2015
+ * Time: 12:03
+ */
+namespace common\modules\generator\rules;
+
+use Yii;
+use common\modules\generator\models\AbstractClassGenerator;
+
+class ActiveRecordDateRuleGenerator extends AbstractActiveRecordRuleGenerator
+{
+    const TYPE = 'date';
+
+    /**
+     * @inheritdoc
+     *
+    public function rules()
+    {
+    return [
+    [['order', 'region_id'], 'integer'],
+    [['title'], 'string', 'max' => 255]
+    [['date'], 'date']
+    ];
+    }*/
+
+    protected function addRules()
+    {
+        $arProperty = $this->getAllPropertiesArray();
+        if(!empty($arProperty)) {
+            $this->rulesString .= "             [[";
+            foreach ($arProperty as $property) {
+                $this->rulesString .= "'" . $property . "', ";
+            }
+            $this->rulesString .= "], '".static::TYPE."'],\n";
+        }
+    }
+
+    protected function getAllPropertiesArray()
+    {
+        $propertiesArray = [];
+        foreach($this->params[AbstractClassGenerator::PROPERTIES] as $property) {
+            if(isset($property[AbstractClassGenerator::PROPERTY_VALIDATION_RULES])) {
+                foreach ($property[AbstractClassGenerator::PROPERTY_VALIDATION_RULES] as $rule) {
+                    if ($rule[AbstractClassGenerator::PROPERTY_VALIDATION_RULE_TYPE] == static::TYPE) {
+                        $propertiesArray[] = $property[AbstractClassGenerator::PROPERTY_NAME];
+                    }
+                }
+            }
+        }
+
+        return $propertiesArray;
+    }
+}
